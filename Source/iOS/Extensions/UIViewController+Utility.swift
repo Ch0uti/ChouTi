65df863be67fdd631e60b34947d185f5fef48e45
Honghao Zhang <zhh358@gmail.com>
2015-08-11 00:46:06 -0400

    Added UIViewController+Utility

---
+++ Source/iOS/Extensions/UIViewController+Utility.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+Utility.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-08-11.
+//
+//
+
+import UIKit
+
+public extension UIViewController {
+	
+	/**
+	Check whether childViewControllers contain a view controller
+	
+	:param: childViewController View controller to be tested
+	
+	:returns: True if contained, false otherwise
+	*/
+	func containChildViewController(childViewController: UIViewController) -> Bool {
+		return self.childViewControllers.filter({$0 as? UIViewController == childViewController}).count > 0
+	}
+}
