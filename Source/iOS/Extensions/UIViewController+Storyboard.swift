db39fb0ac2891ce4e2b236287d02f3554fcc1988
Honghao Zhang <zhh358@gmail.com>
2015-08-11 23:07:00 -0400

    Used spaces as indentations

--- Source/iOS/Extensions/UIViewController+Storyboard.swift
+++ Source/iOS/Extensions/UIViewController+Storyboard.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 public extension UIViewController {
-	
-	/**
-	Initialize a view controller in storyboard.
-	
-	:param: storyboardName     Storyboard name
-	:param: viewControllerName Storyboard ID of the view controller
-	
-	:returns: An instance of view controller.
-	*/
-	class func viewControllerInStoryboard(storyboardName: String , viewControllerName: String) -> UIViewController {
-		var storyboard: UIStoryboard = UIStoryboard(name: storyboardName, bundle: nil)
-		var viewController = storyboard.instantiateViewControllerWithIdentifier(viewControllerName) as! UIViewController
-		return viewController
-	}
+    
+    /**
+    Initialize a view controller in storyboard.
+    
+    :param: storyboardName     Storyboard name
+    :param: viewControllerName Storyboard ID of the view controller
+    
+    :returns: An instance of view controller.
+    */
+    class func viewControllerInStoryboard(storyboardName: String , viewControllerName: String) -> UIViewController {
+        var storyboard: UIStoryboard = UIStoryboard(name: storyboardName, bundle: nil)
+        var viewController = storyboard.instantiateViewControllerWithIdentifier(viewControllerName) as! UIViewController
+        return viewController
+    }
 }
--- Source/iOS/Extensions/UIViewController+Utility.swift
+++ Source/iOS/Extensions/UIViewController+Utility.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 public extension UIViewController {
-	
-	/**
-	Check whether childViewControllers contain a view controller
-	
-	:param: childViewController View controller to be tested
-	
-	:returns: True if contained, false otherwise
-	*/
-	func containChildViewController(childViewController: UIViewController) -> Bool {
-		return self.childViewControllers.filter({$0 as? UIViewController == childViewController}).count > 0
-	}
+    
+    /**
+    Check whether childViewControllers contain a view controller
+    
+    :param: childViewController View controller to be tested
+    
+    :returns: True if contained, false otherwise
+    */
+    func containChildViewController(childViewController: UIViewController) -> Bool {
+        return self.childViewControllers.filter({$0 as? UIViewController == childViewController}).count > 0
+    }
 }
