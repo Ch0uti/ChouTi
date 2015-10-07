a3c020a855ba787b5dc22a8fd55b75b141ae199c
Honghao Zhang <zhh358@gmail.com>
2015-10-07 00:25:33 -0400

    Working version with debug information

--- Source/iOS/UI/MenuPageViewController/PgaeViewController.swift
+++ Source/iOS/UI/MenuPageViewController/PgaeViewController.swift
@@ -44,8 +44,6 @@ public class PageViewController: UIViewController {
 		}
 	}
 	
-	private var forwardAppearanceCalled: Bool = false
-	private var backwardAppearanceCalled: Bool = false
 	
 	private var isDragging: Bool = false {
 		didSet {
@@ -53,9 +51,6 @@ public class PageViewController: UIViewController {
 		}
 	}
 	
-	private var appearanceCalledViewControllers = Set<UIViewController>()
-	private var willAppearViewController: UIViewController?
-	private var willDisappearViewController: UIViewController?
 	
 	private let pageScrollView = UIScrollView()
 	
@@ -64,16 +59,32 @@ public class PageViewController: UIViewController {
 			beginDraggingContentOffsetX = CGFloat(Int(beginDraggingContentOffsetX) / Int(view.bounds.width)) * view.bounds.width
 		}
 	}
+	
+	private var willEndDraggingTargetContentOffset: CGFloat = 0.0
+	
 	private var draggingOffsetX: CGFloat { return pageScrollView.contentOffset.x - beginDraggingContentOffsetX }
 	private var draggingForward: Bool? {
 		if draggingOffsetX == 0 {
 			return nil
-		} else if draggingOffsetX < 0 {
-			return false
 		} else {
-			return true
+			return draggingOffsetX > 0
 		}
 	}
+	
+	private var scrollViewLastContentOffsetX: CGFloat?
+	private var scrollViewMovingForward: Bool? {
+		guard let scrollViewLastContentOffsetX = scrollViewLastContentOffsetX else {
+			return nil
+		}
+		
+		let offset = pageScrollView.contentOffset.x - scrollViewLastContentOffsetX
+		if offset == 0 {
+			return nil
+		} else {
+			return offset > 0
+		}
+	}
+	
 	
 	public override func shouldAutomaticallyForwardAppearanceMethods() -> Bool {
 		return false
@@ -162,101 +173,81 @@ extension PageViewController {
 // MARK: - UIScrollViewDelegate
 extension PageViewController: UIScrollViewDelegate {
 	public func scrollViewDidScroll(scrollView: UIScrollView) {
+//		print("did scroll")
 		if scrollView.contentOffset.y != 0 {
 			scrollView.contentOffset = CGPoint(x: scrollView.contentOffset.x, y: 0)
+		}
+		
+		if !isDragging {
+			let willSelectedIndex = Int(willEndDraggingTargetContentOffset) / Int(view.bounds.width)
+			let willSelectedViewController = viewControllers[willSelectedIndex]
+			
+			if willSelectedViewController.isAppearing == nil {
+				print("sssss")
+				willSelectedViewController.beginAppearanceTransition(true, animated: true)
+			}
+			
+			if selectedViewController.isAppearing == nil {
+				print("sssss123")
+				selectedViewController.beginAppearanceTransition(false, animated: true)
+			}
+			
+			scrollViewLastContentOffsetX = scrollView.contentOffset.x
+			return
 		}
 		
 		// Zero offset
 		guard let draggingForward = draggingForward else {
 			print("dragging zero")
-			if isDragging {
-				if forwardAppearanceCalled {
-					if let forwardViewController = forwardViewController where appearanceCalledViewControllers.contains(forwardViewController) {
-						forwardViewController.beginAppearanceTransition(false, animated: false)
-						forwardViewController.endAppearanceTransition()
-						appearanceCalledViewControllers.remove(forwardViewController)
-					}
-				}
-				
-				if backwardAppearanceCalled {
-					if let backwardViewController = backwardViewController where appearanceCalledViewControllers.contains(backwardViewController) {
-						backwardViewController.beginAppearanceTransition(false, animated: false)
-						backwardViewController.endAppearanceTransition()
-						appearanceCalledViewControllers.remove(backwardViewController)
-					}
-				}
+			if forwardViewController?.isAppearing != nil {
+				forwardViewController?.beginAppearanceTransition(false, animated: false)
+				forwardViewController?.endAppearanceTransition()
 			}
 			
+			if backwardViewController?.isAppearing != nil {
+				backwardViewController?.beginAppearanceTransition(false, animated: false)
+				backwardViewController?.endAppearanceTransition()
+			}
+			
+			scrollViewLastContentOffsetX = scrollView.contentOffset.x
 			return
 		}
 		
+		selectedViewController.beginAppearanceTransition(false, animated: true)
+		
 		if draggingForward {
 			print("dragging forward")
-			if backwardAppearanceCalled {
-				if let backwardViewController = backwardViewController where appearanceCalledViewControllers.contains(backwardViewController) {
-					backwardViewController.beginAppearanceTransition(false, animated: false)
-					backwardViewController.endAppearanceTransition()
-					appearanceCalledViewControllers.remove(backwardViewController)
-				}
-				
-				backwardAppearanceCalled = false
+			if backwardViewController?.isAppearing != nil {
+				backwardViewController?.beginAppearanceTransition(false, animated: false)
+				backwardViewController?.endAppearanceTransition()
 			}
 			
-			if !forwardAppearanceCalled {
-				if !appearanceCalledViewControllers.contains(selectedViewController) {
-					selectedViewController.beginAppearanceTransition(false, animated: true)
-					selectedViewController.appearanceState = .WillDisappear
-					appearanceCalledViewControllers.insert(selectedViewController)
-					willDisappearViewController = selectedViewController
-				}
-				
-				if let forwardViewController = forwardViewController where !appearanceCalledViewControllers.contains(forwardViewController) {
-					forwardViewController.beginAppearanceTransition(true, animated: true)
-					appearanceCalledViewControllers.insert(forwardViewController)
-					willAppearViewController = forwardViewController
-				}
-				
-				forwardAppearanceCalled = true
-			}
+			forwardViewController?.beginAppearanceTransition(true, animated: true)
 		} else {
 			print("dragging backward")
-			if forwardAppearanceCalled {
-				if let forwardViewController = forwardViewController where appearanceCalledViewControllers.contains(forwardViewController) {
-					forwardViewController.beginAppearanceTransition(false, animated: false)
-					forwardViewController.endAppearanceTransition()
-					appearanceCalledViewControllers.remove(forwardViewController)
-				}
-				
-				forwardAppearanceCalled = false
+			if forwardViewController?.isAppearing != nil {
+				forwardViewController?.beginAppearanceTransition(false, animated: false)
+				forwardViewController?.endAppearanceTransition()
 			}
 			
-			if !backwardAppearanceCalled {
-				if !appearanceCalledViewControllers.contains(selectedViewController) {
-					selectedViewController.beginAppearanceTransition(false, animated: true)
-					appearanceCalledViewControllers.insert(selectedViewController)
-					willDisappearViewController = selectedViewController
-				}
-				if let backwardViewController = backwardViewController where !appearanceCalledViewControllers.contains(backwardViewController) {
-					backwardViewController.beginAppearanceTransition(true, animated: true)
-					appearanceCalledViewControllers.insert(backwardViewController)
-					willAppearViewController = backwardViewController
-				}
-				
-				backwardAppearanceCalled = true
-			}
+			backwardViewController?.beginAppearanceTransition(true, animated: true)
 		}
+		
+		scrollViewLastContentOffsetX = scrollView.contentOffset.x
 	}
 	
 	public func scrollViewWillBeginDragging(scrollView: UIScrollView) {
-		appearanceCalledViewControllers.forEach { $0.endAppearanceTransition() }
-		appearanceCalledViewControllers.removeAll()
-		forwardAppearanceCalled = false
-		backwardAppearanceCalled = false
-		if let willAppearViewController = willAppearViewController {
+		print("willBegin dragging")
+		// If when new dragging initiatied, last dragging is still in progress.
+		// End appearance transition immediately
+		// And set selectedIndex to willAppear view controller
+		let appearingViewControllers = viewControllers.filter { $0.isAppearing == true }
+		assert(appearingViewControllers.count <= 1)
+		viewControllers.filter { $0.isAppearing != nil }.forEach { $0.endAppearanceTransition() }
+		
+		if let willAppearViewController = appearingViewControllers.first {
 			selectedIndex = viewControllers.indexOf(willAppearViewController)!
 		}
-		willAppearViewController = nil
-		willDisappearViewController = nil
 		
 		isDragging = true
 		beginDraggingContentOffsetX = scrollView.contentOffset.x
@@ -266,71 +257,145 @@ extension PageViewController: UIScrollViewDelegate {
 	public func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
 		isDragging = false
 		print("target: \(targetContentOffset.memory.x)")
-		if targetContentOffset.memory.x == beginDraggingContentOffsetX {
+		willEndDraggingTargetContentOffset = targetContentOffset.memory.x
+		if willEndDraggingTargetContentOffset == beginDraggingContentOffsetX {
 			// Cancelled
-			if appearanceCalledViewControllers.contains(selectedViewController) {
-				selectedViewController.beginAppearanceTransition(true, animated: true)
-				willAppearViewController = selectedViewController
+			print("revert to initial state")
+			selectedViewController.beginAppearanceTransition(true, animated: true)
+			
+			if forwardViewController?.isAppearing != nil {
+				forwardViewController?.beginAppearanceTransition(false, animated: true)
 			}
 			
-			if let forwardViewController = forwardViewController where appearanceCalledViewControllers.contains(forwardViewController) {
-				forwardViewController.beginAppearanceTransition(false, animated: true)
-				willDisappearViewController = forwardViewController
-			}
-			
-			if let backwardViewController = backwardViewController where appearanceCalledViewControllers.contains(backwardViewController) {
-				backwardViewController.beginAppearanceTransition(false, animated: true)
-				willDisappearViewController = backwardViewController
+			if backwardViewController?.isAppearing != nil {
+				backwardViewController?.beginAppearanceTransition(false, animated: true)
 			}
 		}
 	}
 	
 	public func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
 		// If dragging ends at separator point, clean state
+		print("did end dragging, decelerate: \(decelerate)")
 		if scrollView.contentOffset.x % view.bounds.width == 0 {
-			appearanceCalledViewControllers.forEach { $0.endAppearanceTransition() }
-			appearanceCalledViewControllers.removeAll()
-			forwardAppearanceCalled = false
-			backwardAppearanceCalled = false
-			willAppearViewController = nil
-			willDisappearViewController = nil
+			print("called")
+			viewControllers.filter { $0.isAppearing != nil }.forEach { $0.endAppearanceTransition() }
 			selectedIndex = Int(scrollView.contentOffset.x) / Int(view.bounds.width)
 		}
 	}
 	
 	public func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-		print("end Decel")
-		appearanceCalledViewControllers.forEach { $0.endAppearanceTransition() }
-		appearanceCalledViewControllers.removeAll()
-		forwardAppearanceCalled = false
-		backwardAppearanceCalled = false
-		willAppearViewController = nil
-		willDisappearViewController = nil
+		print("end Decelerating")
 		
-		selectedIndex = Int(scrollView.contentOffset.x) / Int(view.bounds.width)
+		// If for some reasons, scrollView.contentOffset.x is not matched with willEndDraggingTargetContentOffset
+		// End current transitions
+		// Add missing transitions
+		if willEndDraggingTargetContentOffset != scrollView.contentOffset.x {
+			print("special case")
+			let appearingViewControllers = viewControllers.filter { $0.isAppearing == true }
+			assert(appearingViewControllers.count <= 1)
+			
+			// End current transitions
+			viewControllers.filter { $0.isAppearing != nil }.forEach { $0.endAppearanceTransition() }
+			
+			if let willAppearViewController = appearingViewControllers.first {
+				selectedIndex = viewControllers.indexOf(willAppearViewController)!
+				
+				// Add missing transitions
+				selectedViewController.beginAppearanceTransition(false, animated: false)
+				selectedViewController.endAppearanceTransition()
+				
+				let willSelectedIndex = Int(scrollView.contentOffset.x) / Int(view.bounds.width)
+				print("willSelected: \(willSelectedIndex)")
+				let willSelectedViewController = viewControllers[willSelectedIndex]
+				willSelectedViewController.beginAppearanceTransition(true, animated: false)
+				willSelectedViewController.endAppearanceTransition()
+				selectedIndex = willSelectedIndex
+			}
+		} else {
+			print("normal case")
+			viewControllers.filter { $0.isAppearing != nil }.forEach { $0.endAppearanceTransition() }
+			selectedIndex = Int(scrollView.contentOffset.x) / Int(view.bounds.width)
+		}
+		
+		assert(viewControllers.filter { $0.isAppearing != nil }.count == 0)
 	}
 }
 
 extension UIViewController {
+	// AppearanceState Associated Property
 	enum AppearanceState {
 		case WillAppear
 		case WillDisappear
-		case End
 	}
 	
 	private struct AssociatedKeys {
 		static var AppearanceStateKey = "zhh_AppearanceStateKey"
 	}
 	
-	var appearanceState: AppearanceState? {
+	var isAppearing: Bool? {
 		get {
-			return objc_getAssociatedObject(self, &AssociatedKeys.AppearanceStateKey) as? AppearanceState
+			return objc_getAssociatedObject(self, &AssociatedKeys.AppearanceStateKey) as? Bool
 		}
 		
 		set {
-			if let newValue = newValue {
-				objc_setAssociatedObject(self, &AssociatedKeys.AppearanceStateKey, newValue as AppearanceState? as? AnyObject, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+			objc_setAssociatedObject(self, &AssociatedKeys.AppearanceStateKey, newValue as Bool? as? AnyObject, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+		}
+	}
+	
+	// Swizzling beginAppearanceTransition/endAppearanceTransition
+	public override class func initialize() {
+		struct Static {
+			static var beginAppearanceTransitionToken: dispatch_once_t = 0
+			static var endAppearanceTransitionToken: dispatch_once_t = 0
+		}
+		
+		// make sure this isn't a subclass
+		if self !== UIViewController.self {
+			return
+		}
+		
+		dispatch_once(&Static.beginAppearanceTransitionToken) {
+			let originalSelector = Selector("beginAppearanceTransition:animated:")
+			let swizzledSelector = Selector("zhh_beginAppearanceTransition:animated:")
+			
+			let originalMethod = class_getInstanceMethod(self, originalSelector)
+			let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+			
+			let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+			
+			if didAddMethod {
+				class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+			} else {
+				method_exchangeImplementations(originalMethod, swizzledMethod);
 			}
 		}
+		
+		dispatch_once(&Static.endAppearanceTransitionToken) {
+			let originalSelector = Selector("endAppearanceTransition")
+			let swizzledSelector = Selector("zhh_endAppearanceTransition")
+			
+			let originalMethod = class_getInstanceMethod(self, originalSelector)
+			let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+			
+			let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+			
+			if didAddMethod {
+				class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+			} else {
+				method_exchangeImplementations(originalMethod, swizzledMethod);
+			}
+		}
+	}
+	
+	func zhh_beginAppearanceTransition(isAppearing: Bool, animated: Bool) {
+		if self.isAppearing != isAppearing {
+			self.zhh_beginAppearanceTransition(isAppearing, animated: animated)
+			self.isAppearing = isAppearing
+		}
+	}
+	
+	func zhh_endAppearanceTransition() {
+		self.zhh_endAppearanceTransition()
+		self.isAppearing = nil
 	}
 }
