7a259eb7d2bd4fbdd3702cd187f7ce72d1ae9091
Honghao Zhang <zhh358@gmail.com>
2015-10-26 12:19:17 -0400

    Added framework project

---
+++ ChouTi/ChouTi/ChouTi.h
@@ -0,0 +1,19 @@
+//
+//  ChouTi.h
+//  ChouTi
+//
+//  Created by Honghao Zhang on 2015-10-26.
+//  Copyright © 2015 Honghao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChouTi.
+FOUNDATION_EXPORT double ChouTiVersionNumber;
+
+//! Project version string for ChouTi.
+FOUNDATION_EXPORT const unsigned char ChouTiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChouTi/PublicHeader.h>
+
+
---
+++ ChouTi/ChouTiTests/ChouTiTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChouTiTests.swift
+//  ChouTiTests
+//
+//  Created by Honghao Zhang on 2015-10-26.
+//  Copyright © 2015 Honghao Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import ChouTi
+
+class ChouTiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
