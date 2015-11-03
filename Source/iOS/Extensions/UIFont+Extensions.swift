af44df796c6363689e2ba20dab0edaa68315b9fe
Honghao Zhang <zhh358@gmail.com>
2015-11-03 00:42:26 -0500

    Added UIFont extensions

---
+++ Source/iOS/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+Extensions.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-11-02.
+//
+//
+
+import Foundation
+
+public extension UIFont {
+	public class func helveticaNenueFont(fontSize: CGFloat) -> UIFont {
+		return UIFont(name: "HelveticaNeue", size: fontSize)!
+	}
+	
+	public class func helveticaNeueLightFont(fontSize: CGFloat) -> UIFont {
+		return UIFont(name: "HelveticaNeue-Light", size: fontSize)!
+	}
+	
+	public class func helveticaNenueThinFont(fontSize: CGFloat) -> UIFont {
+		return UIFont(name: "HelveticaNeue-Thin", size: fontSize)!
+	}
+}
