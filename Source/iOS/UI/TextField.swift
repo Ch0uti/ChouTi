03d91ad966dd4c26d4b800a8f03d72156c1035f5
Honghao Zhang <zhh358@gmail.com>
2015-11-14 22:56:39 -0500

    Added textfield

---
+++ Source/iOS/UI/TextField.swift
@@ -0,0 +1,21 @@
+//
+//  TextField.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-11-14.
+//
+//
+
+import UIKit
+
+class TextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
