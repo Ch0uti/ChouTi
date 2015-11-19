02cc65daa7c6b282e2458cedb524f16171793d11
Honghao Zhang <zhh358@gmail.com>
2015-11-18 21:47:01 -0500

    Updated TableViewCellInfo

--- Source/iOS/Protocols/TableViewInfo.swift
+++ Source/iOS/Protocols/TableViewInfo.swift
@@ -9,6 +9,11 @@ import UIKit
 
 public protocol TableViewCellInfo {
     static func identifier() -> String
+	// return NSStringFromClass(<#Cell#>.self)
+	
     static func estimatedRowHeight() -> CGFloat
+	// return <#RowHeight#>
+	
     static func registerInTableView(tableView: UITableView)
+	// tableView.registerClass(<#Cell#>.self, forCellReuseIdentifier: <#Cell#>.identifier())
 }
