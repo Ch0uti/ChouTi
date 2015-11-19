b5a8178f01082fb82f17f0550283ae0168ef1aa5
Honghao Zhang <zhh358@gmail.com>
2015-11-18 22:39:17 -0500

    Updated pod spec

--- Source/iOS/Utility/Constants.swift
+++ Source/iOS/Utility/Constants.swift
@@ -8,21 +8,8 @@
 
 import Foundation
 
-#if DEBUG
-public var isIOS7: Bool {
-	return !isIOS8
-	}
-public var isIOS8: Bool {
-	return Tweaks.tweakValueForCategory(
-		"Main Application",
-		collectionName: "Main Application",
-		name: "isIOS8",
-		defaultValue: NSNumber(bool: floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1)).boolValue
-	}
-#else
 public var isIOS7: Bool = !isIOS8
 public let isIOS8: Bool = floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1
-#endif
 
 public var screenWidth: CGFloat { return UIScreen.mainScreen().bounds.size.width }
 public var screenHeight: CGFloat { return UIScreen.mainScreen().bounds.size.height }
@@ -38,5 +25,4 @@ public var isIphone6: Bool { return screenHeight ~= 667.0 }
 public var isIphone6Plus: Bool { return screenHeight ~= 736.0 }
 public var is320ScreenWidth: Bool { return screenWidth ~= 320.0 }
 
-//
-//var keyWindow: UIWindow { return UIApplication.sharedApplication().keyWindow! }
+var keyWindow: UIWindow { return UIApplication.sharedApplication().keyWindow! }
