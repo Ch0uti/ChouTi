7ef5caa8e1733dfc0680a6c438cb5722ddb4e0c3
Honghao Zhang <zhh358@gmail.com>
2015-11-24 22:24:47 -0500

    Updated TextField

--- Source/iOS/UI/TextField.swift
+++ Source/iOS/UI/TextField.swift
@@ -13,6 +13,77 @@ public class TextField: UITextField {
 	public var textHorizontalPadding: CGFloat = 10.0
 	public var textVerticalPadding: CGFloat = 10.0
 	
+	public var cursorColor: UIColor? {
+		didSet {
+			tintColor = cursorColor
+		}
+	}
+	
+	public override var placeholder: String? {
+		didSet {
+			guard let placeholder = placeholder else {
+				return
+			}
+			
+			if placeholderFont != nil || placeholderColor != nil {
+				var attributes = [String : AnyObject]()
+				if let placeholderColor = placeholderColor {
+					attributes[NSForegroundColorAttributeName] = placeholderColor
+				}
+				
+				if let placeholderFont = placeholderFont {
+					attributes[NSFontAttributeName] = placeholderFont
+				}
+				
+				let attributedString = NSAttributedString(string: placeholder, attributes: attributes)
+				
+				attributedPlaceholder = attributedString
+			}
+		}
+	}
+	
+	public var placeholderColor: UIColor? {
+		didSet {
+			guard let placeholder = placeholder else {
+				return
+			}
+			
+			var attributes = [String : AnyObject]()
+			if let placeholderColor = placeholderColor {
+				attributes[NSForegroundColorAttributeName] = placeholderColor
+			}
+			
+			if let placeholderFont = placeholderFont {
+				attributes[NSFontAttributeName] = placeholderFont
+			}
+			
+			let attributedString = NSAttributedString(string: placeholder, attributes: attributes)
+			
+			attributedPlaceholder = attributedString
+		}
+	}
+	
+	public var placeholderFont: UIFont? {
+		didSet {
+			guard let placeholder = placeholder else {
+				return
+			}
+			
+			var attributes = [String : AnyObject]()
+			if let placeholderColor = placeholderColor {
+				attributes[NSForegroundColorAttributeName] = placeholderColor
+			}
+			
+			if let placeholderFont = placeholderFont {
+				attributes[NSFontAttributeName] = placeholderFont
+			}
+			
+			let attributedString = NSAttributedString(string: placeholder, attributes: attributes)
+			
+			attributedPlaceholder = attributedString
+		}
+	}
+	
 	public override func textRectForBounds(bounds: CGRect) -> CGRect {
 		return CGRectInset(bounds, textHorizontalPadding, textVerticalPadding)
 	}
