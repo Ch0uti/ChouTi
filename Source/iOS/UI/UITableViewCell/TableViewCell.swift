e6305c057cfffa3cc8bc899376133efac80a3b9e
Honghao Zhang <zhh358@gmail.com>
2015-11-25 22:53:30 -0500

    Updated TableViewCell

--- Source/iOS/UI/UITableViewCell/TableViewCell.swift
+++ Source/iOS/UI/UITableViewCell/TableViewCell.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 public class TableViewCell: UITableViewCell {
+    public var height: CGFloat = 44.0 {
+        didSet {
+            heightConstraint?.constant = height
+        }
+    }
+    
+    private var heightConstraint: NSLayoutConstraint?
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        heightConstraint = NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0.0, constant: height)
+        heightConstraint!.priority = 999
+        heightConstraint!.active = true
+    }
 }
 
 extension TableViewCell : TableViewCellInfo {
@@ -17,7 +40,7 @@ extension TableViewCell : TableViewCellInfo {
 	}
 	
 	public static func estimatedRowHeight() -> CGFloat {
-		return 60
+		return 44.0
 	}
 	
 	public static func registerInTableView(tableView: UITableView) {
