e9ee715d6ae7ca2010c08a9f0e870364296d2dcb
Honghao Zhang <zhh358@gmail.com>
2015-11-26 23:26:04 -0500

    Added UITextField snippet

---
+++ Source/iOS/CodeSnippets/CodeSnippet_UITextField.swift
@@ -0,0 +1,38 @@
+//
+//  CodeSnippet_UITextField.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-11-26.
+//
+//
+
+
+
+//// MARK: - Setup
+//let <#textFiled#> = TextField()
+//<#textFiled#>.backgroundColor = UIColor(white: 52 / 255, alpha: 1.0)
+//<#textFiled#>.placeholderColor = UIColor(white: 1.0, alpha: 0.36)
+//<#textFiled#>.placeholderFont = UIFont.AvenirMediumFont(12.5 * 2)
+//<#textFiled#>.placeholder = "Placeholder Text"
+//<#textFiled#>.font = UIFont.AvenirMediumFont(12.5 * 2)
+//<#textFiled#>.textColor = UIColor(white: 1.0, alpha: 1.0)
+//<#textFiled#>.cursorColor = UIColor.whiteColor()
+//
+//<#textFiled#>.textVerticalPadding = 8.0
+//<#textFiled#>.textHorizontalPadding = 16.0
+//
+//<#textFiled#>.delegate = self
+//<#textFiled#>.addTarget(self, action: "textFieldDidEdit:", forControlEvents: .EditingChanged)
+
+
+
+//// MARK: - UITextFieldDelegate
+//extension <#ViewController#> : UITextFieldDelegate {
+//	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+//		return true
+//	}
+//	
+//	func textFieldDidEdit(textField: UITextField) {
+//		// TODO: TextField Updated
+//	}
+//}
