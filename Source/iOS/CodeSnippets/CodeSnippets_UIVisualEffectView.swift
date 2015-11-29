7c85e27f48d7fa8af3594867c00819d708e34f9b
Honghao Zhang <zhh358@gmail.com>
2015-11-28 23:29:00 -0500

    Added UIVisual effect view code snippet

---
+++ Source/iOS/CodeSnippets/CodeSnippets_UIVisualEffectView.swift
@@ -0,0 +1,16 @@
+//
+//  UIVisualEffectView.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-11-28.
+//
+//
+
+//let blurEffect = UIBlurEffect(style: .Light)
+//let blurView = UIVisualEffectView(effect: blurEffect)
+//
+//let vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
+//vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+//
+//vibrancyView.contentView.addSubview(<#ViewWithLabels#>)
+//blurView.contentView.addSubview(vibrancyView)
