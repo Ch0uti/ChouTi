c96adf46cb6a51128ffd2321a3a078cf808484b6
Honghao Zhang <zhh358@gmail.com>
2015-11-30 23:05:36 -0500

    Added image asset snippet

---
+++ Source/iOS/CodeSnippets/CodeSnippets_ImageAsset.swift
@@ -0,0 +1,41 @@
+//
+//  CodeSnippets_ImageAsset.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2015-11-29.
+//
+//
+
+//import UIKit
+//
+//extension UIImage {
+//	
+//	enum ImageAssets: String {
+//		case AppIcon = "AppIcon"
+//		case LaunchImage = "LaunchScreenBackground"
+//		case Logo = "Logo"
+//		case LogoColorful = "LogoColorful"
+//		case Add = "Add"
+//		
+//		case EventBackground1 = "EventBackground1"
+//		case EventBackground2 = "EventBackground2"
+//		case EventBackground3 = "EventBackground3"
+//		
+//		case Cutlery = "Cutlery"
+//		case Calendar = "Calendar"
+//		case Beer = "Beer"
+//		
+//		case HeartBreak = "HeartBreak"
+//		case Cross = "Cross"
+//		
+//		case Camera = "Camera"
+//	}
+//	
+//	convenience init!(asset: ImageAssets, inBundle bundle: NSBundle? = nil) {
+//		if let bundle = bundle {
+//			self.init(named: asset.rawValue, inBundle: bundle, compatibleWithTraitCollection: nil)
+//		} else {
+//			self.init(named: asset.rawValue)
+//		}
+//	}
+//}
