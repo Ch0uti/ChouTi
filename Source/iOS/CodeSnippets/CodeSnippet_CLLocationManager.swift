c6ce4026da835810c42de63091a537d628de326c
Honghao Zhang <zhh358@gmail.com>
2015-12-13 23:38:48 -0500

    Added CLLocationManager and Shadow code snippet

---
+++ Source/iOS/CodeSnippets/CodeSnippet_CLLocationManager.swift
@@ -0,0 +1,52 @@
+//
+//  CodeSnippet_CLLocationManager.swift
+//  Pods
+//
+//  Created by Honghao_Zhang on 2015-12-13.
+//
+//
+
+import CoreLocation
+
+// Simulating Locations with Xcode: https://blackpixel.com/writing/2013/05/simulating-locations-with-xcode.html
+// GPX: http://gpx-poi.com
+
+// On iOS8, to request location permission:
+// In info.plist
+// NSLocationAlwaysUsageDescription = "App use Locations service mode Always" (Explanations)
+// NSLocationWhenInUseUsageDescription = "App use Locations service mode In Use" (Explanations)
+
+//let locationManager = CLLocationManager()
+//locationManager.delegate = self
+//locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//locationManager.requestWhenInUseAuthorization()
+//locationManager.startUpdatingLocation()
+
+//extension <#ViewController#> : CLLocationManagerDelegate {
+//	func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//		locationManager.stopUpdatingLocation()
+//		guard let currentLocation = locations.last else {
+//			NSLog("Error: get currentLocation failed")
+//			return
+//		}
+//		
+//		CLGeocoder().reverseGeocodeLocation(currentLocation) { [unowned self] (placemarks, error) -> Void in
+//			if let error = error {
+//				NSLog("Error: Reverse geocoder failed with error: \(error.localizedDescription)")
+//				return
+//			}
+//			
+//			let firstPlace = placemarks?.first
+//			let streetNumber = firstPlace?.subThoroughfare
+//			let streetName = firstPlace?.thoroughfare
+//			
+//			self.addressField.text = (streetNumber != nil ? "\(streetNumber!) " : "") + "\(streetName ?? "")"
+//			
+//			self.cityField.text = firstPlace?.locality ?? firstPlace?.subLocality ?? firstPlace?.subAdministrativeArea ?? firstPlace?.administrativeArea ?? ""
+//		}
+//	}
+//	
+//	func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+//		NSLog("Error: locationManager didFailWithError:" + error.localizedDescription)
+//	}
+//}
---
+++ Source/iOS/CodeSnippets/CodeSnippets_Shadow.swift
@@ -0,0 +1,13 @@
+//
+//  CodeSnippets_Shadow.swift
+//  Pods
+//
+//  Created by Honghao_Zhang on 2015-12-13.
+//
+//
+
+//<#view#>.layer.shadowColor = UIColor.blackColor().CGColor
+//<#view#>.layer.shadowOffset = CGSizeZero
+//<#view#>.layer.shadowOpacity = 0.6
+//<#view#>.layer.shadowRadius = 4.0
+//<#view#>.layer.shadowPath = UIBezierPath(rect: <#view#>.bounds).CGPath
