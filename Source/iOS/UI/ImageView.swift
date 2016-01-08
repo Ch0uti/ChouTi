ae3c6b3b24e33ce665f932c1f389e7a09d7c927b
Honghao Zhang <zhh358@gmail.com>
2016-01-08 18:22:25 -0500

    Added image view

---
+++ Source/iOS/UI/ImageView.swift
@@ -0,0 +1,22 @@
+//
+//  ImageView.swift
+//  Pods
+//
+//  Created by Honghao Zhang on 2016-01-08.
+//
+//
+
+import UIKit
+
+
+public class ImageView: UIImageView {
+	public var rounded: Bool = false
+	
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		if rounded {
+			layer.cornerRadius = min(bounds.width, bounds.height) / 2.0
+		}
+	}
+}
