470fba6e4100e8f935fbfa9f6cbe84ef1dd8f99a
Honghao Zhang <zhh358@gmail.com>
2016-01-12 23:37:30 -0500

    Updated CGRect extensions

--- Source/iOS/Extensions/CGRect+Extensions.swift
+++ Source/iOS/Extensions/CGRect+Extensions.swift
@@ -9,35 +9,62 @@
 import Foundation
 
 public extension CGRect {
+	/// x
 	public var x: CGFloat {
-		return origin.x
+		get { return origin.x }
+		set { origin.x = newValue }
 	}
 	
+	/// y
 	public var y: CGFloat {
-		return origin.y
+		get { return origin.y }
+		set { origin.y = newValue }
 	}
 	
+	/// y
 	public var top: CGFloat {
-		return y
+		get { return y }
+		set { y = newValue }
 	}
 	
+	/// y + height
 	public var bottom: CGFloat {
-		return y + height
+		get { return y + height }
+		set {
+			let newHeight = newValue - top
+			size.height = newHeight > 0 ? newHeight : 0.0
+		}
 	}
 	
-	public var leading: CGFloat {
-		return x
-	}
-	
-	public var trailing: CGFloat {
-		return x + width
-	}
-	
+	/// x
 	public var left: CGFloat {
-		return leading
+		get { return x }
+		set { x = newValue }
 	}
 	
+	/// x + width
 	public var right: CGFloat {
-		return trailing
+		get { return x + width }
+		set {
+			let newWidth = newValue - left
+			size.width = newWidth > 0 ? newWidth : 0.0
+		}
+	}
+	
+	/// Mostly it's left, in Right-to-Left, leading is right
+	public var leading: CGFloat {
+		get { return isRTL ? right : left }
+		set { isRTL ? (right = newValue) : (left = newValue) }
+	}
+	
+	/// Mostly it's right, in Right-to-Left, trailing is left
+	public var trailing: CGFloat {
+		get { return isRTL ? left : right }
+		set { isRTL ? (left = newValue) : (right = newValue) }
+	}
+	
+	/// whether system interfae is Right-to-Left
+	private var isRTL: Bool {
+		return UIApplication.sharedApplication().userInterfaceLayoutDirection == .RightToLeft
 	}
 }
