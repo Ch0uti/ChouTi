522fb589139f3f2eb5c90bfac92801e03f7182a7
Honghao Zhang <zhh358@gmail.com>
2016-01-23 13:49:14 -0500

    Added documentation

--- Source/iOS/Extensions/NSTimeZone+Extensions.swift
+++ Source/iOS/Extensions/NSTimeZone+Extensions.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 public extension NSTimeZone {
+	/**
+	Get GMT time zone
+	
+	- returns: GMT time zone
+	*/
 	public static func GMT() -> NSTimeZone {
 		guard let gmt = NSTimeZone(abbreviation: "GMT") else {
 			NSLog("Error: time zone with `GMT` abbreviation not found")
@@ -17,6 +22,11 @@ public extension NSTimeZone {
 		return gmt
 	}
 	
+	/**
+	Get EST time zone
+	
+	- returns: EST time zone
+	*/
 	public static func EST() -> NSTimeZone {
 		guard let est = NSTimeZone(abbreviation: "EST") else {
 			NSLog("Error: time zone with `EST` abbreviation not found")
