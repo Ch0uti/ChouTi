3a1488577534df133eed84a44b58b4c9da7b6110
Honghao Zhang <zhh358@gmail.com>
2016-01-30 22:21:30 -0500

    Updated

--- ChouTi/ChouTiTests/ExtensionsTests/String+ExtensionsTests.swift
+++ ChouTi/ChouTiTests/ExtensionsTests/String+ExtensionsTests.swift
@@ -6,11 +6,11 @@
 //  Copyright © 2015 Honghao Zhang. All rights reserved.
 //
 
-import XCTest
+//import XCTest
 //import ChouTi
-
+//
 //class String_ExtensionsTest: ChouTiTests {
-//	
+//
 //	override func setUp() {
 //		super.setUp()
 //		// Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,11 +20,11 @@ import XCTest
 //		// Put teardown code here. This method is called after the invocation of each test method in the class.
 //		super.tearDown()
 //	}
-//	
-//	func testFullNSRange() {
-//		let string = "123456abc"
-//		let nsRange = string.fullNSRange()
-//		XCTAssertEqual(nsRange.location, 0)
-//		XCTAssertEqual(nsRange.length, 9)
-//	}
+////
+////	func testFullNSRange() {
+////		let string = "123456abc"
+////		let nsRange = string.fullNSRange()
+////		XCTAssertEqual(nsRange.location, 0)
+////		XCTAssertEqual(nsRange.length, 9)
+////	}
 //}
