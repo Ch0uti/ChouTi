2b62baba22dfdb024d816f5f1fd5e875eba92c38
Honghao Zhang <zhh358@gmail.com>
2016-04-29 22:28:16 -0400

    Update gitignore and clean tracked files

--- ChouTi_iOS_Example/Pods/Target Support Files/ChouTi/ChouTi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ChouTi : NSObject
-@end
-@implementation PodsDummy_ChouTi
-@end
--- ChouTi_iOS_Example/Pods/Target Support Files/ChouTi/ChouTi-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double ChouTiVersionNumber;
-FOUNDATION_EXPORT const unsigned char ChouTiVersionString[];
-
