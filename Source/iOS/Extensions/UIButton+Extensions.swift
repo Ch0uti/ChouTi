9158f1c2a7b77ad7fce8d51d9906ffe3458c1389
Honghao Zhang <zhh358@gmail.com>
2016-05-07 22:37:27 -0400

    Added documentations

--- Source/iOS/Extensions/UIButton+Extensions.swift
+++ Source/iOS/Extensions/UIButton+Extensions.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 public extension UIButton {
+    
+    /**
+     Sets the background color to use for the specified state.
+     
+     - parameter color: The background color to use for the specified state.
+     - parameter state: The state that uses the specified background color. The possible values are described in UIControlState.
+     */
     public func setBackgroundColor(color: UIColor, forState state: UIControlState) {
         self.setBackgroundImage(UIImage.imageWithColor(color), forState: state)
     }
