38747b45fff524c40638660694b431ac5f7c5e4c
Honghao Zhang <zhh358@gmail.com>
2016-06-29 14:41:46 -0400

    Add test files

---
+++ ChouTi/ChouTiTests/ExtensionsTests/NSDate+ExtensionsTests.swift
@@ -0,0 +1,31 @@
+//
+//  NSDate+ExtensionsTests.swift
+//  ChouTi_FrameworkTests
+//
+//  Created by Honghao Zhang on 2015-12-13.
+//  Copyright © 2015 Honghao Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import ChouTi
+
+class NSDate_ExtensionsTests: ChouTiTests {
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+	
+	func testDateComparison() {
+		XCTAssertTrue(NSDate().isEarlierThanDate(NSDate(timeIntervalSinceNow: 20)))
+	}
+	
+	func testForSettingUnit() {
+		let date = NSDate()
+		let dateWithUpdatedYear = date.dateBySettingUnit(.Year, newValue: 1900)
+		XCTAssertEqual(dateWithUpdatedYear?.year, 1900)
+	}
+}
