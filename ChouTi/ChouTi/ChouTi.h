f26037b0d1c3c4a1506323750e30858b974d5e9b
Honghao Zhang <zhh358@gmail.com>
2016-06-29 13:47:45 -0400

    Create ChouTi framework

---
+++ ChouTi/ChouTi/ChouTi.h
@@ -0,0 +1,19 @@
+//
+//  ChouTi.h
+//  ChouTi
+//
+//  Created by Honghao Zhang on 2016-06-29.
+//  Copyright © 2016 Honghaoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChouTi.
+FOUNDATION_EXPORT double ChouTiVersionNumber;
+
+//! Project version string for ChouTi.
+FOUNDATION_EXPORT const unsigned char ChouTiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChouTi/PublicHeader.h>
+
+
