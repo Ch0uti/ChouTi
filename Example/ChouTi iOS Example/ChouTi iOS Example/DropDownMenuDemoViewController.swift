38b6be94879f8db02485f7e2174803098a93e005
Honghao Zhang <zhh358@gmail.com>
2016-06-29 23:35:08 -0400

    Setup new ChouTi iOS Example project

---
+++ Example/ChouTi iOS Example/ChouTi iOS Example/DropDownMenuDemoViewController.swift
@@ -0,0 +1,110 @@
+//
+//  DropDownMenuDemoViewController.swift
+//  ChouTi iOS Example
+//
+//  Created by Honghao Zhang on 2015-12-03.
+//  Copyright © 2015 Honghao Zhang. All rights reserved.
+//
+
+import UIKit
+import ChouTi
+
+class DropDownMenuDemoViewController: UIViewController {
+
+	let navigationBarMenu = DropDownMenu()
+	let smallMenu = DropDownMenu()
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		view.backgroundColor = UIColor.whiteColor()
+    }
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		if let navigationBar = navigationController?.navigationBar {
+			navigationBarMenu.setHidden(true)
+			
+			navigationBarMenu.translatesAutoresizingMaskIntoConstraints = false
+			navigationBarMenu.dataSource = self
+			navigationBarMenu.delegate = self
+			
+			navigationBarMenu.selectedIndex = 2
+			
+			view.addSubview(navigationBarMenu)
+			
+			var constraints = [NSLayoutConstraint]()
+			
+			// navigationBarMenu
+			constraints += [NSLayoutConstraint(item: navigationBarMenu, attribute: .Top, relatedBy: .Equal, toItem: navigationBar, attribute: .Bottom, multiplier: 1.0, constant: 0.0)]
+			constraints += [NSLayoutConstraint(item: navigationBarMenu, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 0.0)]
+			constraints += [NSLayoutConstraint(item: navigationBarMenu, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1.0, constant: 0.0)]
+			
+			constraints += [NSLayoutConstraint(item: navigationBarMenu, attribute: .Height, relatedBy: .Equal, toItem: navigationBar, attribute: .Height, multiplier: 1.0, constant: 0.0)]
+			
+			// smallMenu
+			smallMenu.translatesAutoresizingMaskIntoConstraints = false
+			smallMenu.dataSource = self
+			smallMenu.delegate = self
+			smallMenu.selectedIndex = 0
+			smallMenu.backgroundColor = UIColor.purpleColor()
+			smallMenu.statusBarStyle = .LightContent
+			view.addSubview(smallMenu)
+			
+			constraints += [NSLayoutConstraint(item: smallMenu, attribute: .Top, relatedBy: .Equal, toItem: navigationBar, attribute: .Bottom, multiplier: 1.0, constant: 100.0)]
+			constraints += [NSLayoutConstraint(item: smallMenu, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 100.0)]
+			constraints += [NSLayoutConstraint(item: smallMenu, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1.0, constant: -100.0)]
+			
+			constraints += [NSLayoutConstraint(item: smallMenu, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0.0, constant: 60.0)]
+			
+			NSLayoutConstraint.activateConstraints(constraints)
+			
+			navigationBarMenu.setHidden(false, animated: true)
+		}
+	}
+	
+	override func viewWillDisappear(animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		navigationBarMenu.setHidden(true, animated: true, duration: 1.0, completion: { finished in
+			self.navigationBarMenu.removeFromSuperview()
+		})
+	}
+}
+
+extension DropDownMenuDemoViewController : DropDownMenuDataSource {
+	func numberOfOptionsInDropDownMenu(dropDownMenu: DropDownMenu) -> Int {
+		return 6
+	}
+	
+	func dropDownMenu(dropDownMenu: DropDownMenu, optionTitleForIndex index: Int) -> String {
+		return "Option \(index)"
+	}
+}
+
+extension DropDownMenuDemoViewController : DropDownMenuDelegate {
+	func dropDownMenu(dropDownMenu: DropDownMenu, willSelectIndex index: Int) {
+		print("dropDownMenu willSelectIndex: \(index)")
+	}
+	
+	func dropDownMenu(dropDownMenu: DropDownMenu, didSelectIndex index: Int) {
+		print("dropDownMenu didSelectIndex: \(index)")
+	}
+	
+	func dropDownMenuWillExpand(dropDownMenu: DropDownMenu) {
+		print("dropDownMenuWillExpand")
+	}
+	
+	func dropDownMenuDidExpand(dropDownMenu: DropDownMenu) {
+		print("dropDownMenuDidExpand")
+	}
+	
+	func dropDownMenuWillCollapse(dropDownMenu: DropDownMenu) {
+		print("dropDownMenuWillCollapse")
+	}
+	
+	func dropDownMenuDidCollapse(dropDownMenu: DropDownMenu) {
+		print("dropDownMenuDidCollapse")
+	}
+}
---
+++ Example/ChouTi iOS Example/ChouTi iOS Example/PageViewDemoController.swift
@@ -0,0 +1,96 @@
+//
+//  PageViewDemoController.swift
+//  ChouTi iOS Example
+//
+//  Created by Honghao Zhang on 2015-09-29.
+//  Copyright © 2015 Honghao Zhang. All rights reserved.
+//
+
+import UIKit
+import ChouTi
+
+class PageViewDemoController: UIViewController {
+	
+	let pageViewController = PageViewController()
+	var dummyViewControllers: [UIViewController]!
+	
+    override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.backgroundColor = UIColor.whiteColor()
+		
+		let vc1 = SideViewController(nibName: "SideViewController", bundle: nil)
+		vc1.view.backgroundColor = UIColor.random()
+		vc1.label.text = "vc1"
+		vc1.view.frame = UIScreen.mainScreen().bounds
+		
+		let vc2 = SideViewController(nibName: "SideViewController", bundle: nil)
+		vc2.view.backgroundColor = UIColor.random()
+		vc2.label.text = "vc2"
+		vc2.view.frame = UIScreen.mainScreen().bounds
+		
+		let vc3 = SideViewController(nibName: "SideViewController", bundle: nil)
+		vc3.view.backgroundColor = UIColor.random()
+		vc3.label.text = "vc3"
+		vc3.view.frame = UIScreen.mainScreen().bounds
+		
+		let vc4 = SideViewController(nibName: "SideViewController", bundle: nil)
+		vc4.view.backgroundColor = UIColor.random()
+		vc4.label.text = "vc4"
+		
+		dummyViewControllers = [vc1, vc2, vc3, vc4]
+		
+		// demoMenuPageViewController.viewControllers = dummyViewControllers
+		
+		pageViewController.dataSource = self
+		pageViewController.delegate = self
+		
+//		self.automaticallyAdjustsScrollViewInsets = false
+		
+		addChildViewController(pageViewController)
+//		let frame = pageViewController.view.frame
+//		pageViewController.view.frame = CGRect(x: frame.origin.x, y: frame.origin.y + 22 + 44, width: frame.width, height: frame.height)
+		view.addSubview(pageViewController.view)
+		pageViewController.didMoveToParentViewController(self)
+		
+		pageViewController.setSelectedIndex(2, animated: true)
+	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		delay(0.5) { _ in
+			self.pageViewController.setSelectedIndex(1, animated: true)
+		}
+		
+		delay(1.0) { _ in
+			self.pageViewController.setSelectedIndex(3, animated: true)
+		}
+		
+		delay(4.0) { _ in
+			self.dummyViewControllers! += self.dummyViewControllers!
+			print("self.dumycount: \(self.dummyViewControllers.count)")
+		}
+	}
+}
+
+extension PageViewDemoController : PageViewControllerDataSource {
+	func numberOfViewControllersInPageViewController(pageViewController: PageViewController) -> Int {
+		return dummyViewControllers.count
+	}
+	
+	func pageViewController(pageViewController: PageViewController, viewControllerForIndex index: Int) -> UIViewController {
+		print("asking for index: \(index)")
+		return dummyViewControllers[index]
+	}
+}
+
+extension PageViewDemoController : PageViewControllerDelegate {
+	func pageViewController(pageViewController: PageViewController, didSelectIndex selectedIndex: Int, selectedViewController: UIViewController) {
+		print("did selected: \(selectedIndex)")
+	}
+	
+	func pageViewController(pageViewController: PageViewController, didScrollWithSelectedIndex selectedIndex: Int, offsetPercent: CGFloat) {
+		print("scroll offset: \(offsetPercent)")
+	}
+}
