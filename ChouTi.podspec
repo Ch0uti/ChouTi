1b4d55a332def35eea74dc1386f144dbf533df4e
Honghao Zhang <zhh358@gmail.com>
2016-11-17 16:52:17 -0500

    0.2

--- ChouTi.podspec
+++ ChouTi.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "ChouTi"
-  s.version          = "0.1"
+  s.version          = "0.2"
   s.summary          = "Chou Ti (抽屉) - A toolkit for iOS/OSX development."
   s.description      = <<-DESC
                        Chou Ti (抽屉) - My personal toolkit for iOS/OSX development.
--- Source/iOS/Core/Extensions/NSLayoutAnchor+Extensions.swift
+++ Source/iOS/Core/Extensions/NSLayoutAnchor+Extensions.swift
@@ -8,43 +8,86 @@
 
 import Foundation
 
-public extension NSLayoutAnchor {
-    public func constrainTo(anchor: NSLayoutAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+public extension NSLayoutXAxisAnchor {
+	@discardableResult
+    public func constrain(to anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(equalTo: anchor, constant: constant).activate()
     }
-    
-    public func constrainGreaterThanOrEqualTo(anchor: NSLayoutAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(greaterThanOrEqualTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(greaterThanOrEqualTo: anchor, constant: constant).activate()
     }
-    
-    public func constrainLessThanOrEqualTo(anchor: NSLayoutAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(lessThanOrEqualTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(lessThanOrEqualTo: anchor, constant: constant).activate()
     }
 }
 
+public extension NSLayoutYAxisAnchor {
+	@discardableResult
+	public func constrain(to anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(equalTo: anchor, constant: constant).activate()
+	}
+	
+	@discardableResult
+	public func constrain(greaterThanOrEqualTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(greaterThanOrEqualTo: anchor, constant: constant).activate()
+	}
+	
+	@discardableResult
+	public func constrain(lessThanOrEqualTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(lessThanOrEqualTo: anchor, constant: constant).activate()
+	}
+}
+
+public extension NSLayoutDimension {
+	@discardableResult
+	public func constrain(to anchor: NSLayoutDimension, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(equalTo: anchor, constant: constant).activate()
+	}
+	
+	@discardableResult
+	public func constrain(greaterThanOrEqualTo anchor: NSLayoutDimension, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(greaterThanOrEqualTo: anchor, constant: constant).activate()
+	}
+	
+	@discardableResult
+	public func constrain(lessThanOrEqualTo anchor: NSLayoutDimension, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+		return self.constraint(lessThanOrEqualTo: anchor, constant: constant).activate()
+	}
+}
+
 public extension NSLayoutDimension {
 
-    public func constrainTo(constant: CGFloat) -> NSLayoutConstraint {
+	@discardableResult
+    public func constrain(to constant: CGFloat) -> NSLayoutConstraint {
         return self.constraint(equalToConstant: constant).activate()
     }
-    
-    public func constrainGreaterThanOrEqualTo(constant: CGFloat) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(greaterThanOrEqualToConstant constant: CGFloat) -> NSLayoutConstraint {
         return self.constraint(greaterThanOrEqualToConstant: constant).activate()
     }
-    
-    public func constrainLessThanOrEqualTo(onstant constant: CGFloat) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(lessThanOrEqualToConstant constant: CGFloat) -> NSLayoutConstraint {
         return self.constraint(lessThanOrEqualToConstant: constant).activate()
     }
-    
-    public func constrainTo(anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(to anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(equalTo: anchor, multiplier: multiplier, constant: constant).activate()
     }
-    
-    public func constrainGreaterThanOrEqualTo(anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(greaterThanOrEqualTo anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(greaterThanOrEqualTo: anchor, multiplier: multiplier, constant: constant).activate()
     }
-    
-    public func constrainLessThanOrEqualTo(anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
+	
+	@discardableResult
+    public func constrain(lessThanOrEqualTo anchor: NSLayoutDimension, multiplier: CGFloat, constant: CGFloat = 0.0) -> NSLayoutConstraint {
         return self.constraint(lessThanOrEqualTo: anchor, multiplier: multiplier, constant: constant).activate()
     }
 }
