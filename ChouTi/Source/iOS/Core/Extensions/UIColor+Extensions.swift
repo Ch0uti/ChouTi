cc5000506bace49490003c86da1c31927f80f080
Honghao Zhang <zhh358@gmail.com>
2018-05-21 15:49:33 -0700

    Fix deprecation warnings in UIColor+Extension

--- ChouTi/Source/iOS/Core/Extensions/UIColor+Extensions.swift
+++ ChouTi/Source/iOS/Core/Extensions/UIColor+Extensions.swift
@@ -67,10 +67,8 @@ public extension UIColor {
     }
 }
 
-
-
 public extension UIColor {
-    public class func random(_ randomAlpha: Bool = false) -> UIColor {
+    public class func random(randomAlpha: Bool = false) -> UIColor {
         let randomRed = CGFloat.random()
         let randomGreen = CGFloat.random()
         let randomBlue = CGFloat.random()
@@ -143,45 +141,138 @@ public extension UIColor {
 }
 
 // MARK: - HEX Colors
-// Optional Pod:
-//   pod 'UIColor_Hex_Swift' ( https://github.com/yeahdongcn/UIColor-Hex-Swift )
-//   pod 'SwiftHEXColors' ( https://github.com/thii/SwiftHEXColors )
+
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 R0CKSTAR
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 public extension UIColor {
     /**
-     UIColor(hexString: "#CC0000")
-     
-     - parameter hexString: hexString, e.g. "#CC0000"
-     
-     - returns: UIColor
+     The shorthand three-digit hexadecimal representation of color.
+     #RGB defines to the color #RRGGBB.
+
+     - parameter hex3: Three-digit hexadecimal value.
+     - parameter alpha: 0.0 - 1.0. The default is 1.0.
+     */
+    public convenience init(hex3: UInt16, alpha: CGFloat = 1) {
+        let divisor = CGFloat(15)
+        let red     = CGFloat((hex3 & 0xF00) >> 8) / divisor
+        let green   = CGFloat((hex3 & 0x0F0) >> 4) / divisor
+        let blue    = CGFloat( hex3 & 0x00F      ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    /**
+     The shorthand four-digit hexadecimal representation of color with alpha.
+     #RGBA defines to the color #RRGGBBAA.
+
+     - parameter hex4: Four-digit hexadecimal value.
+     */
+    public convenience init(hex4: UInt16) {
+        let divisor = CGFloat(15)
+        let red     = CGFloat((hex4 & 0xF000) >> 12) / divisor
+        let green   = CGFloat((hex4 & 0x0F00) >>  8) / divisor
+        let blue    = CGFloat((hex4 & 0x00F0) >>  4) / divisor
+        let alpha   = CGFloat( hex4 & 0x000F       ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    /**
+     The six-digit hexadecimal representation of color of the form #RRGGBB.
+
+     - parameter hex6: Six-digit hexadecimal value.
+     */
+    public convenience init(hex6: UInt32, alpha: CGFloat = 1) {
+        let divisor = CGFloat(255)
+        let red     = CGFloat((hex6 & 0xFF0000) >> 16) / divisor
+        let green   = CGFloat((hex6 & 0x00FF00) >>  8) / divisor
+        let blue    = CGFloat( hex6 & 0x0000FF       ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    /**
+     The six-digit hexadecimal representation of color with alpha of the form #RRGGBBAA.
+
+     - parameter hex8: Eight-digit hexadecimal value.
+     */
+    public convenience init(hex8: UInt32) {
+        let divisor = CGFloat(255)
+        let red     = CGFloat((hex8 & 0xFF000000) >> 24) / divisor
+        let green   = CGFloat((hex8 & 0x00FF0000) >> 16) / divisor
+        let blue    = CGFloat((hex8 & 0x0000FF00) >>  8) / divisor
+        let alpha   = CGFloat( hex8 & 0x000000FF       ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    /**
+     The rgba string representation of color with alpha of the form #RRGGBBAA/#RRGGBB.
+
+     - parameter hexString: String value represents rgba values.
      */
     public convenience init?(hexString: String) {
         guard hexString.hasPrefix("#") else {
             return nil
         }
-        guard hexString.characters.count == "#000000".characters.count else {
+
+        let hexString: String = String(hexString[String.Index.init(encodedOffset: 1)...])
+        var hexValue:  UInt32 = 0
+
+        guard Scanner(string: hexString).scanHexInt32(&hexValue) else {
             return nil
         }
-        let digits = hexString.substring(from: hexString.characters.index(hexString.startIndex, offsetBy: 1))
-        guard Int(digits, radix: 16) != nil else {
+
+        switch (hexString.count) {
+        case 3:
+            self.init(hex3: UInt16(hexValue))
+        case 4:
+            self.init(hex4: UInt16(hexValue))
+        case 6:
+            self.init(hex6: hexValue)
+        case 8:
+            self.init(hex8: hexValue)
+        default:
             return nil
         }
-        let red = digits.substring(to: digits.characters.index(digits.startIndex, offsetBy: 2))
-        let green = digits.substring(with: digits.characters.index(digits.startIndex, offsetBy: 2) ..< digits.characters.index(digits.startIndex, offsetBy: 4))
-        let blue = digits.substring(with: digits.characters.index(digits.startIndex, offsetBy: 4) ..< digits.characters.index(digits.startIndex, offsetBy: 6))
-        let redf = CGFloat(Double(Int(red, radix: 16)!) / 255.0)
-        let greenf = CGFloat(Double(Int(green, radix: 16)!) / 255.0)
-        let bluef = CGFloat(Double(Int(blue, radix: 16)!) / 255.0)
-        self.init(red: redf, green: greenf, blue: bluef, alpha: CGFloat(1.0))
     }
-    
-    /// Get Hex6 String, e.g. "#CC0000"
-    public var hexString: String {
-        let colorRef = self.cgColor.components
-        
-        let r: CGFloat = colorRef![0]
-        let g: CGFloat = colorRef![1]
-        let b: CGFloat = colorRef![2]
-        
-        return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+
+    /**
+     Hex string of a UIColor instance, fails to nil.
+
+     - parameter includeAlpha: Whether the alpha should be included.
+     */
+    public func hexString(includeAlpha: Bool = true) -> String? {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+
+        guard r >= 0 && r <= 1 && g >= 0 && g <= 1 && b >= 0 && b <= 1 else {
+            return nil
+        }
+
+        if (includeAlpha) {
+            return String(format: "#%02X%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255), Int(a * 255))
+        } else {
+            return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+        }
     }
 }
