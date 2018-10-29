37c3ec4019345250933050eed82e70235e9cd0ec
Honghao Zhang <honghaoz90@gmail.com>
2018-10-29 14:13:23 -0700

    Update build tool chain

---
+++ ChouTi/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if a command exists
+function command_exists() { [ -x "$(command -v $1)" ]; }
+
+function install_gems() {
+  if ! command_exists bundler; then
+  	echo "Install 'bundler'"
+    sudo gem install bundler
+  fi
+
+  bundle check >/dev/null 2>&1 || bundle install
+}
+
+install_gems
