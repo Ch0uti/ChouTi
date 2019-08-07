a95bdee0793aa85a2b08c913dc8268c36c229393
HongHao Zhang <3926785+honghaoz@users.noreply.github.com>
2019-08-07 14:04:29 -0700

    Fix uiscrollView scrolls to the bottom

--- Sources/iOS/Extensions/UIScrollView+Extensions.swift
+++ Sources/iOS/Extensions/UIScrollView+Extensions.swift
@@ -4,21 +4,22 @@ import UIKit
 
 public extension UIScrollView {
     /**
-     Scrolls ScrollView to the top
+     Scrolls to the top.
 
-     - parameter animated: whether it's animated
+     - parameter animated: whether it's animated.
      */
     func scrollsToTop(animated: Bool) {
         setContentOffset(CGPoint(x: contentOffset.x, y: -contentInset.top), animated: animated)
     }
 
     /**
-     Scrolls ScrollView to the bottom
+     Scrolls to the bottom with content inset included.
 
-     - parameter animated: whether it's animated
+     - parameter animated: whether it's animated.
      */
     func scrollsToBottom(animated: Bool) {
-        let bottomOffset = CGPoint(x: contentOffset.x, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        let bottomOffset = CGPoint(x: contentOffset.x,
+                                   y: contentSize.height - bounds.height + adjustedContentInset.bottom)
         setContentOffset(bottomOffset, animated: animated)
     }
 }
